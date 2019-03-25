5c7f0d24d18eaf593a48f3276932f673a02c435c
Elemen.Z <element.zhan@gmail.com>
2019-03-25 16:28:55 +0800

    add turning left and right using the keyboard function and reconstruct the code

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,21 +1,22 @@
-import sys
-
+from ship import Ship
 import pygame
 
 from settings import Settings
+import game_functions as gf
 
 
 def run_game():
     pygame.init()
     ai_settings=Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    ship=Ship(ai_settings,screen)
+
     pygame.display.set_caption("Alien Invasion")
     bg_color=(230,230,230)
+
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(ai_settings.bg_color)
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship)
 
 run_game()
