e26551fe94af5dbaa00477e2eeb501ec6c38123b
elemen.z <element.zhan@gmail.com>
2019-03-27 18:55:03 +0800

    add fire function to the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 from ship import Ship
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 import game_functions as gf
@@ -10,13 +11,15 @@ def run_game():
     ai_settings=Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     ship=Ship(ai_settings,screen)
+    bullets=Group()
 
     pygame.display.set_caption("Alien Invasion")
     bg_color=(230,230,230)
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
+        bullets.update()
+        gf.update_screen(ai_settings,screen,ship,bullets)
 
 run_game()
